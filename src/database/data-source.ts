06fa2e59ebfba7cb0fb1ba3d42968b6f6407dabe
Devesh <devesh.bag2@gmail.com>
2022-11-15 08:34:18 +0800

    Change path of sqlite file

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -3,7 +3,7 @@ import { DataSource, Db } from "typeorm"
 import path from 'path'
 import Book from "../entity/Book"
 
-const root = path.resolve(__dirname, "..")
+const root = path.resolve(__dirname, "../../")
 const db = new DataSource({
     type: 'sqlite',
     database: `${root}/db.sqlite`,
